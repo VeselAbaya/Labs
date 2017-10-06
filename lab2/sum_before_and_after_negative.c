bc4f0e7635d724540e9fe27b1103b040c56dae8b
VeselAbaya <Vandervise465@gmail.com>
2017-10-06 18:30:53 +0300

    #

--- lab2/sum_before_and_after_negative.c
+++ lab2/sum_before_and_after_negative.c
@@ -8,7 +8,7 @@ int sum_before_and_after_negative(int const a[], int size) {
 
   int sum = 0;
   
-  for (int i = 0; i != index_first_negative(a, size) + 1; ++i) {
+  for (int i = 0; i != index_first_negative(a, size); ++i) {
     sum += abs(a[i]);
   }
   for (int i = index_last_negative(a, size); i != size; ++i) {
