ff2d7aca33fcb08fcc671099c8f3bd7409a04e64
VeselAbaya <Vandervise465@gmail.com>
2017-10-06 15:25:57 +0300

    #

--- lab2/index_last_negative.c
+++ lab2/index_last_negative.c
@@ -1,11 +1,7 @@
 #include "index_last_negative.h"
 
 int index_last_negative(int const a[], int size) {
-  int ret = 0;
-
-  for (int i = 0; i != size; ++i) {
-    if (a[i] < 0) { ret = i; }
+  for (int i = size - 1; i != -1; --i) {
+    if (a[i] < 0) { return i; }
   }
-
-  return ret;
 }
